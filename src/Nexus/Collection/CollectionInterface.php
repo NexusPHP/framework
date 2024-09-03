f3d31c0b5ee3a7187a4961ad5c51a7c05542a1a5
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2024-09-03 21:33:22 +0800

    Add `CollectionInterface::wrap()`

---
+++ src/Nexus/Collection/CollectionInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Nexus framework.
+ *
+ * (c) John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Nexus\Collection;
+
+/**
+ * @template TKey
+ * @template T
+ *
+ * @extends \IteratorAggregate<TKey, T>
+ */
+interface CollectionInterface extends
+    \Countable,
+    \IteratorAggregate
+{
+    /**
+     * @template WKey
+     * @template W
+     *
+     * @param (\Closure(): iterable<WKey, W>)|iterable<WKey, W> $items
+     *
+     * @return self<WKey, W>
+     */
+    public static function wrap(\Closure|iterable $items): self;
+}
