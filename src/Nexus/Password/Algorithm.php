da5468947e35063d5e452f2548b9b72a15edb960
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2024-12-17 21:36:49 +0800

    Add `Algorithm` enum

---
+++ src/Nexus/Password/Algorithm.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the Nexus framework.
+ *
+ * (c) John Paul E. Balandan, CPA <paulbalandan@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Nexus\Password;
+
+enum Algorithm: string
+{
+    case Bcrypt = PASSWORD_BCRYPT;
+    case Argon2i = PASSWORD_ARGON2I;
+    case Argon2id = PASSWORD_ARGON2ID;
+}
