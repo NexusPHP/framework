80bf6af4a18fcbe94ec5aadcb00e5bc2f0c25bff
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2024-12-21 19:56:07 +0800

    `Password::fromAlgorithm` to take on string algorithm instead

--- src/Nexus/Password/Password.php
+++ src/Nexus/Password/Password.php
@@ -18,6 +18,7 @@ final readonly class Password
     /**
      * Creates a password hash driver.
      *
+     * @param non-empty-string $algo
      * @param array{
      *  cost?: int,
      *  iterations?: int,
@@ -31,8 +32,10 @@ final readonly class Password
      *
      * @throws HashException
      */
-    public static function fromAlgorithm(Algorithm $algorithm, array $options = []): HashInterface
+    public static function fromAlgorithm(string $algo, array $options = []): HashInterface
     {
+        $algorithm = Algorithm::tryFrom($algo);
+
         return match ($algorithm) {
             Algorithm::Argon2i => new Hash\Argon2iHash($algorithm, $options),
             Algorithm::Argon2id => new Hash\Argon2idHash($algorithm, $options),
@@ -40,7 +43,8 @@ final readonly class Password
             Algorithm::Pbkdf2HmacSha1,
             Algorithm::Pbkdf2HmacSha256,
             Algorithm::Pbkdf2HmacSha512 => new Hash\Pbkdf2Hash($algorithm, $options),
-            default => new Hash\SodiumHash($algorithm, $options),
+            Algorithm::Sodium => new Hash\SodiumHash($algorithm, $options),
+            default => throw new HashException(\sprintf('Unsupported "%s" algorithm.', $algo)),
         };
     }
 }
--- tests/Password/PasswordTest.php
+++ tests/Password/PasswordTest.php
@@ -13,12 +13,12 @@ declare(strict_types=1);
 
 namespace Nexus\Tests\Password;
 
-use Nexus\Password\Algorithm;
 use Nexus\Password\Hash\Argon2idHash;
 use Nexus\Password\Hash\Argon2iHash;
 use Nexus\Password\Hash\BcryptHash;
 use Nexus\Password\Hash\Pbkdf2Hash;
 use Nexus\Password\Hash\SodiumHash;
+use Nexus\Password\HashException;
 use Nexus\Password\Password;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
@@ -33,12 +33,17 @@ final class PasswordTest extends TestCase
 {
     public function testFactory(): void
     {
-        self::assertInstanceOf(Argon2iHash::class, Password::fromAlgorithm(Algorithm::Argon2i));
-        self::assertInstanceOf(Argon2idHash::class, Password::fromAlgorithm(Algorithm::Argon2id));
-        self::assertInstanceOf(BcryptHash::class, Password::fromAlgorithm(Algorithm::Bcrypt));
-        self::assertInstanceOf(Pbkdf2Hash::class, Password::fromAlgorithm(Algorithm::Pbkdf2HmacSha1));
-        self::assertInstanceOf(Pbkdf2Hash::class, Password::fromAlgorithm(Algorithm::Pbkdf2HmacSha256));
-        self::assertInstanceOf(Pbkdf2Hash::class, Password::fromAlgorithm(Algorithm::Pbkdf2HmacSha512));
-        self::assertInstanceOf(SodiumHash::class, Password::fromAlgorithm(Algorithm::Sodium));
+        self::assertInstanceOf(Argon2iHash::class, Password::fromAlgorithm(PASSWORD_ARGON2I));
+        self::assertInstanceOf(Argon2idHash::class, Password::fromAlgorithm(PASSWORD_ARGON2ID));
+        self::assertInstanceOf(BcryptHash::class, Password::fromAlgorithm(PASSWORD_BCRYPT));
+        self::assertInstanceOf(BcryptHash::class, Password::fromAlgorithm(PASSWORD_DEFAULT));
+        self::assertInstanceOf(Pbkdf2Hash::class, Password::fromAlgorithm('sha1'));
+        self::assertInstanceOf(Pbkdf2Hash::class, Password::fromAlgorithm('sha256'));
+        self::assertInstanceOf(Pbkdf2Hash::class, Password::fromAlgorithm('sha512'));
+        self::assertInstanceOf(SodiumHash::class, Password::fromAlgorithm('sodium'));
+
+        $this->expectException(HashException::class);
+        $this->expectExceptionMessage('Unsupported "md5" algorithm.');
+        Password::fromAlgorithm('md5');
     }
 }
